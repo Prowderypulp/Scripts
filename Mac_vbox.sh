790132409784d1428361cfe7c29905d725db0b9d
Chris S <62338341+Chinmay6@users.noreply.github.com>
2020-06-22 01:04:00 -0400

    Add files via upload

---
+++ Mac_vbox.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd "C:\Program Files\Oracle\VirtualBox\"
+VBoxManage.exe modifyvm "Catalina" --cpuidset 00000001 000106e5 00100800 0098e3fd bfebfbff
+VBoxManage setextradata "Catalina" "VBoxInternal/Devices/efi/0/Config/DmiSystemProduct" "iMac11,3"
+VBoxManage setextradata "Catalina" "VBoxInternal/Devices/efi/0/Config/DmiSystemVersion" "1.0"
+VBoxManage setextradata "Catalina" "VBoxInternal/Devices/efi/0/Config/DmiBoardProduct" "Iloveapple"
+VBoxManage setextradata "Catalina" "VBoxInternal/Devices/smc/0/Config/DeviceKey" "ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
+VBoxManage setextradata "Catalina" "VBoxInternal/Devices/smc/0/Config/GetKeyFromRealSMC" 1
+echo"You are done"
